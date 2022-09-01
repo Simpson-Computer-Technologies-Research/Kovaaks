fdd1a5ccc82cc37edd4dad1d214e491359856a5b
tristan <heytristaann@gmail.com>
2022-08-31 20:12:36 -0400

    Cleaned up code that was written over 2 years ago :O

--- js/scroll.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    var controller = new ScrollMagic.Controller();
-    var ourScene = new ScrollMagic.Scene({
-        triggerElement: '.r6logo2',
-        reverse: false
-    })
-    .setClassToggle('.r6logo2', 'show')
-    .addTo(controller);
-})
