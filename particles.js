71bdea7cc523da0cb0262b14856cdca0ff8d0c93
Tristan <75189508+realTristan@users.noreply.github.com>
2022-09-25 19:38:11 -0400

    Create particles.js

---
+++ particles.js
@@ -0,0 +1,52 @@
+{
+    "particles":{
+        "number":{
+            "value":200
+        },
+        "color":{
+            "value":"#fff"
+
+              },
+            "shape":{
+            "type":"circle",
+            "stroke":{
+                "width":1,
+                "color" : "#ccc"
+            }
+        },
+        "opacity":{
+            "value":1,
+            "random":true,
+            "anim":{
+                "enable":true,
+                "speed":1
+
+            }
+        },
+        "size":{
+            "value": 3,
+            "random":true
+            
+        }
+    },
+    "interactivity":{
+        "events":{
+            "onhover":{
+                "enable":true,
+                "mode":"repulse"
+            },
+            "onclick":{
+                "enable":true,
+                "mode":"push"
+
+            }
+        },
+        "modes":{
+            "repulse":{
+                "distance":90,
+                "duration":0.4
+            }
+        }
+    }
+
+}
